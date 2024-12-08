38aa8839e4b535bf4aaa7273111332fa21c66f8e
Elizabeth Cyr <elizkimcyr@gmail.com>
2024-12-07 19:09:28 -0800

    Fixed attack combogit aa!

--- scavengerTestingGrounds/Assets/Scripts/StateMachine/PlayerStateMachine.cs
+++ scavengerTestingGrounds/Assets/Scripts/StateMachine/PlayerStateMachine.cs
@@ -182,7 +182,7 @@ public struct ComboStep
 
 public class PlayerMainAttackState : PlayerBaseState
 {
-    private const float AnimationDampTime = 0.1f;
+    //private const float AnimationDampTime = 0.1f;
     private const float CrossFadeDuration = 0.1f; //the time it should take for one animation to transition to the next
     //private float animationTime = 0f; //tracks time spent in main attack state
     //private VisualEffect BladeHitEffect;
@@ -193,8 +193,6 @@ public class PlayerMainAttackState : PlayerBaseState
     //bool isBladeSlashVfxTriggered = false;
     //AnimationEventReceiver receiver;
 
-
-    // TODO: STEWART vars
     public static ComboStep[] comboSteps = {
         new("MainAttack1", Animator.StringToHash("MainAttack1"), 0.4f, 1.0f),
         new("MainAttack2", Animator.StringToHash("MainAttack2"), 0.16f, 0.8f), // TODO: roughly 0.2f for attack anim
@@ -208,15 +206,11 @@ public class PlayerMainAttackState : PlayerBaseState
 
     public override void Enter()
     {
-
-        Debug.Log("main attack pressed");
-
         curComboStepIdx = 0;
         comboStepTime = 0f;
         doNextComboStep = false;
         //isBladeSlashVfxTriggered = false;
 
-        Debug.Log("STEWART: STARTING ATTACK ANIM FROM BEGINNING");
         FaceMoveDirection();
 
         stateMachine.Animator.SetBool(comboSteps[0].animParam, true);
@@ -234,11 +228,6 @@ public class PlayerMainAttackState : PlayerBaseState
      * CONSTANTS
      */
 
-    // TODO(sdsmith): sdkjfsldjfksldjf
-    // if the button was pressed again within the "combo step window", mark to move to the next combo step.
-    // if min anim time has passed for current combo step and should move to next combo step, start next combo step
-    // otherwise, do combo step 1 (ie start the attack anim from the beginning)
-
     // NOTE(sdsmith): 
     // TODO
     // IMPORTANT
@@ -247,28 +236,20 @@ public class PlayerMainAttackState : PlayerBaseState
 
     public override void Tick()
     {
-        Debug.Log("Tick: start Tick");
-        Debug.Log($"Tick: Animator - normalizeTime={stateMachine.Animator.GetCurrentAnimatorStateInfo(0).normalizedTime}");
-        Debug.Log($"Tick: Animator - current running anim: '{stateMachine.Animator.GetCurrentAnimatorClipInfo(0)[0].clip.name}'");
-
         comboStepTime += Time.deltaTime; // NOTE: A += B => A = A + B
         ref ComboStep curStep = ref comboSteps[curComboStepIdx];
 
         if (comboStepTime <= curStep.attackAnimEndTime 
             && stateMachine.InputReader.IsMainAttackActionTriggered())
         {
-            Debug.Log($"STEWART: Queue next anim step: {curComboStepIdx} -> {curComboStepIdx+1}");
             // Queue up the next combo step to be triggered
             doNextComboStep = true;
         }
 
         if (comboStepTime >= curStep.attackAnimEndTime) {
-            Debug.Log($"STEWART: attack anim done for step {curComboStepIdx}");
             bool onFinalComboStep = curComboStepIdx >= comboSteps.Length - 1;
             if (doNextComboStep && !onFinalComboStep)
             {
-                Debug.Log("STEWART: transition to next step");
-
                 // Continue the combo, go to next combo step
                 stateMachine.Animator.SetBool(comboSteps[curComboStepIdx].animParam, false);
                 curComboStepIdx++;
@@ -282,17 +263,16 @@ public class PlayerMainAttackState : PlayerBaseState
             }
             else if (comboStepTime >= curStep.fullAnimEndTime)
             {
-                Debug.Log($"STEWART: transition to idle anim from step {curComboStepIdx}");
-                // Done full anim, transition to idle
-                stateMachine.SwitchState(new PlayerMoveState(stateMachine));
+                // Done full anim, transition to idle state
+               
+                doNextComboStep = false;
+                stateMachine.Animator.SetBool(curStep.animParam, false);
+                stateMachine.SwitchState(new PlayerMoveState(stateMachine)); // TODO: memory leak? Are we supposed to being doing new and will the old idle state object get cleaned up??
             }
         }
-        Debug.Log("Tick: end Tick");
     }
 
     public override void Exit() {
-        Debug.Log("STEWART: EXIT BIIIIIIIIIIIITCH");
-        
     }
 
     /*
